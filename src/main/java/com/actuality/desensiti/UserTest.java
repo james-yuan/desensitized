10667efccf18310d6ea5f24b0e66467c49d73032
yuanzk <zm212896@163.com>
2024-11-18 10:16:31 +0800

    Create UserTest.java

---
+++ src/main/java/com/actuality/desensiti/UserTest.java
@@ -0,0 +1,36 @@
+package com.actulity.desensiti;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setId(45454511);
+        user.setName("张xxxx");
+        user.setHomeIp("232.75.33.12");
+        user.setYyy("第三方的科技管理的国家的怒然");
+        User user1 = ObjectConvert.parseObject(user, User.class);
+        System.out.println(JSONObject.toJSONString(user1));
+    }
+
+    @Data
+    static
+    class User {
+
+        @Desensitized(type = SensitiveTypeEnum.ID_CARD)
+        private int id;
+
+
+        @Desensitized(type = SensitiveTypeEnum.CHINESE_NAME)
+        private String name;
+
+        @Desensitized(type = SensitiveTypeEnum.MOBILE_PHONE)
+        private String homeIp;
+
+        @Desensitized(type = SensitiveTypeEnum.ADDRESS)
+        private String yyy;
+    }
+
+}
